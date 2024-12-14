9c877116c0f3a39121198a32ee56f7e1bc06ea50
Tristan Oern <sigges.86@gmail.com>
2024-12-14 02:07:17 +0100

    Create script.js
    added javascript file

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add interactivity to the button
+document.getElementById("actionBtn").addEventListener("click", function () {
+  document.getElementById("responseText").innerText = "You clicked the button! 🎉";
+});
